bc1e947193a8966707794e8d06223630d39bde50
luixaviles <luis.avilesz@gmail.com>
2019-04-20 23:49:11 -0400

    Cleaning service

--- src/app/movies/shared/services/movie.service.ts
+++ src/app/movies/shared/services/movie.service.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Movie } from '../model/movie';
 import { HttpClient } from '@angular/common/http';
-import { Observable, pipe } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-
 const MOVIES_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc';
-const MOVIE_API = 'https://api.themoviedb.org/3/movie';
 const API_KEY = '0971ed323ba8081b990144eef9e02ace';
-
 const PICTURE_URL = 'https://image.tmdb.org/t/p/w500';
-
 
 @Injectable({
   providedIn: 'root'
